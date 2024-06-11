653c7638d2154f2cca4b23a86b4e0a56f550441e
Justin Hui <jhui@lcs.on.ca>
2024-06-11 13:24:54 -0400

    Fixed the font sizes in the cover page

--- CYOATemplate/Views/CoverView.swift
+++ CYOATemplate/Views/CoverView.swift
@@ -31,7 +31,7 @@ struct CoverView: View {
                     
                     // Show the cover
                     Text("Secrets in the Halls")
-                        .font(Font.custom("Chalkduster", size: 49))
+                        .font(Font.custom("Chalkduster", size: 50))
                         .foregroundColor(Color.red)
                     
                     Button("Begin The Adventure!") {
